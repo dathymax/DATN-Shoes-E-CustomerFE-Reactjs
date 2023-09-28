0814074b30a8f0943f1a262cab6cf45ff9a87348
nguyenvandat <nguyenvandat@gmail.com>
2023-09-28 21:03:37 +0700

    update axios

--- src/config/axios/index.ts
+++ src/config/axios/index.ts
@@ -1,23 +1,10 @@
 import axios from "axios";
-import Cookies from "js-cookie";
-
-const token = Cookies.get("USER-AUTH") || localStorage.getItem("USER-AUTH");
-
-const axiosConfig = {
-    headers: {
-        'content-Type': 'application/json',
-        "Accept": "/",
-        "Cache-Control": "no-cache",
-        "Cookie": document.cookie
-    },
-    credentials: "same-origin"
-};
-
-axios.defaults.withCredentials = true;
 
 const Axios_instance = axios.create({
     baseURL: "http://localhost:8000/api/v1",
-    ...axiosConfig
+    headers: {
+        'content-Type': 'application/json',
+    },
 });
 
 Axios_instance.interceptors.request.use(config => {
