5851abae8b0ffb750d485e7a80e31ab2e98c4c70
datnguyen <datnv@godgroup.co>
2023-12-14 16:13:04 +0700

    update

--- src/components/product/cart/InTransaction.tsx
+++ src/components/product/cart/InTransaction.tsx
@@ -1,16 +1,19 @@
 import { FC } from "react";
 import { IPurchasedProduct } from "../../../types";
+import { UPLOAD_URL } from "../../../constants";
 
 interface ProductCartInTransactionProps {
     product?: IPurchasedProduct;
 }
 
-const ProductCartInTransaction: FC<ProductCartInTransactionProps> = ({ product }) => {
+const ProductCartInTransaction: FC<ProductCartInTransactionProps> = ({
+    product,
+}) => {
     return (
         <div className="grid grid-cols-3 gap-3">
             <div className="col-span-1 h-[150px]">
                 <img
-                    src={product?.image?.toString()}
+                    src={`${UPLOAD_URL}/${product?.image?.toString()}`}
                     alt="Product"
                     className="h-full w-full rounded-md object-cover"
                 />
@@ -21,9 +24,13 @@ const ProductCartInTransaction: FC<ProductCartInTransactionProps> = ({ product }
                     <p className="font-medium text-[16px]">{product?.name}</p>
                 </div>
                 <div className="ml-4 flex items-center w-full text-[16px] mb-2">
-                    {product?.size} | <p className="capitalize ml-1 flex items-center gap-2">
+                    {product?.size} |{" "}
+                    <p className="capitalize ml-1 flex items-center gap-2">
                         {product?.color}
-                        <p className="w-[25px] h-[25px] rounded-lg" style={{ background: product?.color }}></p>
+                        <p
+                            className="w-[25px] h-[25px] rounded-lg"
+                            style={{ background: product?.color }}
+                        ></p>
                     </p>
                 </div>
             </div>
